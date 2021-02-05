9eeae9841fada0ffa5b5b590f7bf260d5691d50a
Hyunwoo Kwak <ktkwak5218@naver.com>
2021-02-05 17:32:09 +0900

    210205

--- BOJ/Test.py
+++ BOJ/Test.py
@@ -1,7 +1,16 @@
-a = input()
-count = 0
-for i in a:
-    if count % 10 == 0:
-        print()
-    print(i, end = '')
-    count += 1
+n = int(input())
+
+result = n
+for i in range(n):
+    check = input()
+    checked_alphabets = []
+    prev = check[0]
+
+    for j in check:
+        if prev != j and j in checked_alphabets:
+            result -= 1
+            break
+        if j not in checked_alphabets:
+            checked_alphabets.append(j)
+        prev = j
+print(result)
